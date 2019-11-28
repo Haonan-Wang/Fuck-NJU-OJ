20ecbf6334750feecaa7afc527f0d1935c25d5ef
zhengr <ruizheng1119@gmail.com>
2019-11-28 23:57:35 +0800

    加入问题描述

--- 非递归快排.py
+++ 非递归快排.py
@@ -1,3 +1,19 @@
+"""
+非递归快排
+Description
+
+快速排序的核心思想是使用元素的值对数组进行划分。实现其非递归方案。
+
+
+Input
+
+输入的每一行表示一个元素为正整数的数组，所有值用空格隔开，第一个值为数值长度，其余为数组元素值。
+
+
+Output
+
+输出的每一行为排序结果，用空格隔开，末尾不要空格。
+"""
 def partition(nums, begin, end):
     pivot = nums[begin]
     while begin < end:
