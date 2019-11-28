a6d4fb4a596f08acbd469222f92786648335983f
Haonan-Wang <527725618@qq.com>
2019-11-28 23:24:59 +0800

    add description

--- 距离问题.py
+++ 距离问题.py
@@ -3,7 +3,6 @@
 Description
 
 In a given cartesian plane, there are N points. We need to find the Number of Pairs of points(A,B) such that
-
 Point A and Point B do not coincide.
 Manhattan Distance and the Euclidean Distance between the points should be equal.
 Note : Pair of 2 points(A,B) is considered same as Pair of 2 points(B,A).
@@ -14,6 +13,8 @@ Euclidean Distance = ((x2-x1)^2 + (y2-y1)^2)^0.5 where points are (x1,y1) and (x
 
 Constraints:1<=T <= 50, 1<=N <= 2*10 ^ 5, 0<=(|Xi|, |Yi|) <= 10^9
 
+【机翻】
+在给定的笛卡尔平面中，有N个点。 我们需要找到点对（A，B）的对数，以使点A和点B不重合。 点之间的曼哈顿距离和欧几里得距离应相等。
 
 Input
 
