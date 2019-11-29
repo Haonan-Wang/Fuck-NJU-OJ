5f53d18de9b4011d64cbf7e01f9df229bc8d411d
tangzc <3302940372@qq.com>
2019-11-29 10:57:56 +0800

    加入问题描述

--- 汉诺塔.py
+++ 汉诺塔.py
@@ -1,3 +1,30 @@
+"""
+汉诺塔
+
+Description
+
+汉诺塔问题中限制不能将一层塔直接从最左侧移动到最右侧，也不能直接从最右侧移动到最左侧，而是必须经过中间。求当有N层塔的时候移动步数。
+
+
+Input
+
+输入第一行为用例个数， 每个测试用例输入的第一行为N。
+
+
+Output
+
+移动步数。
+
+
+Sample Input 1
+
+1
+2
+Sample Output 1
+
+8
+"""
+
 def func(n, fro, buf, to):
     if (n == 1):
         return 2
