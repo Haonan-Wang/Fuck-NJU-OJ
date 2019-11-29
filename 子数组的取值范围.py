31f8f88518dce293d5b68810c1ed380edf8cd031
tangzc <3302940372@qq.com>
2019-11-29 10:57:56 +0800

    加入问题描述

--- 子数组的取值范围.py
+++ 子数组的取值范围.py
@@ -1,3 +1,31 @@
+"""
+子数组的取值范围
+
+Description
+
+给定数组arr和整数num，求arr的连续子数组中满足：其最大值减去最小值的结果大于num的个数。请实现一个时间复杂度为O(length(arr))的算法。
+
+
+Input
+
+输入第一行为测试用例个数。每一个用例有若干行，第一行为数组，每一个数用空格隔开，第二行为num。
+
+
+Output
+
+输出一个值。
+
+
+Sample Input 1
+
+1
+3 6 4 3 2
+2
+Sample Output 1
+
+6
+"""
+
 if __name__ == "__main__":
     T = int(input())
     while T:
